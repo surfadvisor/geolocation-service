edd563fc66f738519fe884f0b00325e50f6cdb88
poznas <misterdannypl@gmail.com>
2019-10-04 21:52:55 +0200

    init with basic aws dynamoDB setup

---
+++ application/src/main/java/com/surf/advisor/spot/SpotApplication.java
@@ -0,0 +1,13 @@
+package com.surf.advisor.spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotApplication.class, args);
+	}
+
+}
